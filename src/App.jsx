d4bf7c1dff0bcfbd5df4528f587f32cc4c9b2afb
Tran Vinh Thuan <thuan.tran@bstarsolutions.com>
2025-07-16 15:13:52 +0700

    register gsap, ScrollTrigger, SplitText

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+// ---register gsap
+import {ScrollTrigger, SplitText} from 'gsap/all'
 import React from 'react';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
